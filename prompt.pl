102d816c0bb58aa78b9e90be03a747ef5a731c0f
Matthew <MatthewBrien@outlook.com>
2016-03-01 15:10:23 -0500

    Print madlib in terminal

--- prompt.pl
+++ prompt.pl
@@ -1,19 +1,15 @@
 #!usr/local/bin/perl
 use strict;
 #use warnings;
-
 print("Hello Madlibs\n");
 my $date = time();
 #uncomment next line for unique names otherwise, use the line after next
 #my $output_file = "madlib$date.txt";
 my $output_file = "mad.txt";
-
 unless(open OUTPUT, ">$output_file") {
         print( "\nUnable to create '$output_file'\n");
         return 0;
-
     }
-
   #idea, have a header value at the top, that flags if a madlib is complete or not
   #idea two, never save over the original file, always write to a new one.
   #TODO allow user to pass a template file to the program
@@ -30,10 +26,8 @@ my @match;
 my $i =0;
 #for each line of the template, create an array of the regular expression matches
  while(!eof(MADLIB)) {
-
    $line = <MADLIB>;
     @match = ($line =~ /([_]+[A-Za-z\ \-]+[_]+)/g);
-
     foreach (@match)
     {
         #prompt user- then replace in line, and print line to new_file
@@ -43,14 +37,15 @@ my $i =0;
         $temp =~ s/\n//g;
         $line =~ s/$_/$temp/g; #swap array element and entered
         $i++;
-
     }
     print OUTPUT $line;
-
     }
 print "MAdlib complete! $i words replaced \n";
-
 #TODO don't save until completion
-
   close MADLIB;
   close OUTPUT;
+  open OUTPUT, "<$output_file";
+  while(!eof(OUTPUT)){
+    $line = <OUTPUT>;
+    print $line;
+  }
